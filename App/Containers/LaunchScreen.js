517abe6549a8b619d01f1aa938633721813e37bf
Trần Hữu Quang <kieron.qtran@gmail.com>
2018-04-14 21:57:17 +0700

    switch computer

--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -5,26 +5,12 @@ import { Button } from 'react-native-elements'
 // import Icon from 'react-native-vector-icons/FontAwesome'
 // Styles
 import styles from './Styles/LaunchScreenStyles'
-
+import ClothDetailScreen from "./ClothDetailScreen";
 export default class LaunchScreen extends Component {
   render () {
     return (
       <View style={styles.mainContainer}>
-        <Image source={Images.background} style={styles.backgroundImage} resizeMode='stretch' />
-        <ScrollView style={styles.container}>
-          <View style={styles.centered}>
-            <Image source={Images.launch} style={styles.logo} />
-          </View>
-
-          <View style={styles.section} >
-            <Image source={Images.ready} />
-            <Text style={styles.sectionText}>
-              {/* <Icon name="rocket" size={30} color="#900" /> */}
-              This probably isn't what your app is going to look like. Unless your designer handed you this screen and, in that case, congrats! You're ready to ship. For everyone else, this is where you'll see a live preview of your fully functioning app using Ignite.
-            </Text>
-          </View>
-          <Button title="Hello Ignite" />
-        </ScrollView>
+      <ClothDetailScreen/>
       </View>
     )
   }
---
+++ App/Containers/Styles/ClothDetailScreenStyle.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native'
+import { ApplicationStyles, Colors, Fonts, Metrics } from '../../Themes/'
+import { Text } from 'react-native-elements';
+
+export default StyleSheet.create({
+  //container: {
+  //  alignItems: 'center',
+  //},
+
+  title: {
+    fontFamily: 'monospace',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 30,
+    marginVertical: Metrics.doubleBaseMargin
+
+
+
+  }
+});
